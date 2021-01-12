399b5c04d34a29b6a47428ac6d9dd973066e55ec
samuel <samuelagyekumhene@gmail.com>
2021-01-12 06:23:27 +0000

    done

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import router from "./router";
 import store from "./store";
 import LoadScript from "vue-plugin-load-script";
 import "./registerServiceWorker";
+import "./registerServiceWorker";
 
 Vue.use(LoadScript);
 
